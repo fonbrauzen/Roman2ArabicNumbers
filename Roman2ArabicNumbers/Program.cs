e0ea7854221c36283e3f5dcc371cdccad9d8adb5
Tim Ivanchuk <fonbrauzen@gmail.com>
2018-04-28 18:48:22 +0200

    Add project files.

---
+++ Roman2ArabicNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roman2ArabicNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
